c1bcfa4bb0c412832adb895b431e4e759b2d6d07
milan <milanpaulson8@gmail.com>
2023-10-18 21:36:20 +0530

    fix build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Cabin } from 'next/font/google'
-import Navbar from './components/Navbar'
-import Script from 'next/script'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Cabin } from "next/font/google";
+import Navbar from "./components/Navbar";
+import Script from "next/script";
 
-
-
-const cabin = Cabin({ subsets: ['latin'], style: ['normal'] })
+const cabin = Cabin({ subsets: ["latin"], style: ["normal"] });
 
 export const metadata: Metadata = {
-  title: 'Green Haven',
-  description: 'Sustainable home designs ready at your fingertips',
-}
+  title: "Green Haven",
+  description: "Sustainable home designs ready at your fingertips",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-
   return (
     <html lang="en">
       <body className={cabin.className}>
         <Navbar />
         {children}
-        <Script>
-          {`
-            let html = document.querySelector('body');
-            html.style.height = window.innerHeight.toString();
-          `}
-        </Script>
       </body>
     </html>
-  )
+  );
 }
