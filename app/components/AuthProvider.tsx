a41d876b4a766bdf4905382ff7cb8ea8648781a2
milan <milanpaulson8@gmail.com>
2023-10-19 21:26:53 +0530

    fix sign in issue

--- app/components/AuthProvider.tsx
+++ app/components/AuthProvider.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider basePath={process.env.NEXTAUTH_URL}>
+      {children}
+    </SessionProvider>
+  );
 };
 
 export default AuthProvider;
