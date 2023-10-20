1c19bd34791e4afd4a1dac698f8cbb17796526f5
milan <milanpaulson8@gmail.com>
2023-10-20 18:57:28 +0530

    add google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import users from "@/app/helpers/constants";
 import { url } from "inspector";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -15,7 +16,7 @@ const authOptions: NextAuthOptions = {
         },
       },
 
-      // @ts-ignore
+      //@ts-ignore
       async authorize(credentials, req) {
         if (!credentials || !credentials.username || !credentials.password) {
           return null;
@@ -30,7 +31,29 @@ const authOptions: NextAuthOptions = {
         }
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
+        return (
+          // @ts-ignore
+          profile?.email_verified && profile.email?.endsWith("@gmail.com")
+        );
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
