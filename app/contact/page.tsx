e8bbec4848a3392aa40f0c962f7150f2bfea7a97
milan <milanpaulson8@gmail.com>
2023-12-14 00:24:58 +0530

    add home designs

--- app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React, { useEffect, useState } from "react";
-
-const ContactPage = () => {
-  return <div>ContactPage</div>;
-};
-
-export default ContactPage;
--- app/helpers/constants.ts
+++
@@ -1,6 +0,0 @@
-const users = [
-  { id: 1, name: "milan", email: "milan@test.com", password: "123" },
-  { id: 2, name: "nalim", email: "nalim@test.com", password: "123456" },
-];
-
-export default users;
