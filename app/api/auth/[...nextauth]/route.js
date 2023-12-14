5515c6974272f16174cbae4d1cc0ffe09f33bf92
milan <milanpaulson8@gmail.com>
2023-12-14 14:32:21 +0530

    .

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -53,7 +53,7 @@ const authOptions = {
         },
       },
       profile(profile) {
-        return { role: profile.role ?? "user", ...profile };
+        return { ...profile, role: profile.role ?? "user" };
       },
     }),
   ],
