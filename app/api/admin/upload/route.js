10e936f76a187416d8748ae96739949f5f782d0c
Milan Paulson <milanpaulson8@gmail.com>
2024-03-08 23:45:15 +0530

    fix warning issue

--- app/api/admin/upload/route.js
+++ app/api/admin/upload/route.js
@@ -2,12 +2,14 @@ import { NextResponse } from "next/server.js";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import prisma from "../../../lib/prismaClient.js";
 import s3 from "../../../lib/s3client.js";
+process.noDeprecation = true;
 
 export async function POST(request) {
   const formData = await request.formData();
   const image = formData.get("image");
   const buffer = await new Response(image).arrayBuffer();
   const buff = Buffer.from(buffer);
+  console.log(buff);
 
   const params = {
     Bucket: process.env.BUCKET_NAME,
