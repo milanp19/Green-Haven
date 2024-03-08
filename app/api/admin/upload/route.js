65d945697c402bfb5af230faa250303fa3a83f5d
Milan Paulson <milanpaulson8@gmail.com>
2024-03-09 00:30:40 +0530

    make upload routes runtime to edge

--- app/api/admin/upload/route.js
+++ app/api/admin/upload/route.js
@@ -2,14 +2,12 @@ import { NextResponse } from "next/server.js";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import prisma from "../../../lib/prismaClient.js";
 import s3 from "../../../lib/s3client.js";
-process.noDeprecation = true;
 
 export async function POST(request) {
   const formData = await request.formData();
   const image = formData.get("image");
   const buffer = await new Response(image).arrayBuffer();
   const buff = Buffer.from(buffer);
-  console.log(buff);
 
   const params = {
     Bucket: process.env.BUCKET_NAME,
@@ -31,3 +29,5 @@ export async function POST(request) {
 export async function GET(req) {
   return NextResponse.json("drg");
 }
+
+export const runtime = "edge";
